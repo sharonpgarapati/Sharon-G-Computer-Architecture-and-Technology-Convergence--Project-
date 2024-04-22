fcc8292ca0fd18d3f0fcaa5481cb6e5c5a8f538e
sharonpgarapati <sharonpgarapati@gmail.com>
2024-04-22 00:11:43 +0000

    Created linuxCommands.sh and added some content

---
+++ linuxCommands.sh
@@ -0,0 +1,66 @@
+
+
+ 1. # Sharon Garapati
+ 2. # A shell script that automates execution of below commands and outputs the result into a text file.
+ 3. echo "" >> sharonGarapati.txt
+ 4. echo "============" >> sharonGarapati.txt
+ 5. echo "date Command" >> sharonGarapati.txt
+ 6. echo "============" >> sharonGarapati.txt
+ 7. echo "" >> sharonGarapati.txt
+ 8. date >> sharonGarapati.txt
+ 9. echo "" >> sharonGarapati.txt
+10. echo "================" >> sharonGarapati.txt
+11. echo "hostname Command" >> sharonGarapati.txt
+12. echo "================" >> sharonGarapati.txt
+13. echo "" >> sharonGarapati.txt
+14. hostname >> sharonGarapati.txt
+15. echo "" >> sharonGarapati.txt
+16. echo "============" >> sharonGarapati.txt
+17. echo "arch Command" >> sharonGarapati.txt
+18. echo "============" >> sharonGarapati.txt
+19. echo "" >> sharonGarapati.txt
+20. arch >> sharonGarapati.txt
+21. echo "" >> sharonGarapati.txt
+22. echo "========" >> sharonGarapati.txt
+23. echo "uname -a" >> sharonGarapati.txt
+24. echo "========" >> sharonGarapati.txt
+25. echo "" >> sharonGarapati.txt
+26. uname -a >> sharonGarapati.txt
+27. echo "" >> sharonGarapati.txt
+28. echo "===============" >> sharonGarapati.txt
+29. echo " uptime Command" >> sharonGarapati.txt
+30. echo "===============" >> sharonGarapati.txt
+31. echo "" >> sharonGarapati.txt
+32. uptime >> sharonGarapati.txt
+33. echo "" >> sharonGarapati.txt
+34. echo "==============" >> sharonGarapati.txt
+35. echo "whoami Command" >> sharonGarapati.txt
+36. echo "==============" >> sharonGarapati.txt
+37. echo "" >> sharonGarapati.txt
+38. whoami >> sharonGarapati.txt
+39. echo "" >> sharonGarapati.txt
+40. echo "===========" >> sharonGarapati.txt
+41. echo "who Command" >> sharonGarapati.txt
+42. echo "===========" >> sharonGarapati.txt
+43. echo "" >> sharonGarapati.txt
+44. who >> sharonGarapati.txt
+45. echo "" >> sharonGarapati.txt
+46. echo "=========" >> sharonGarapati.txt
+47. echo "w Command" >> sharonGarapati.txt
+48. echo "=========" >> sharonGarapati.txt
+49. echo "" >> sharonGarapati.txt
+50. w >> sharonGarapati.txt
+51. echo "" >> sharonGarapati.txt
+52. echo "===========" >> sharonGarapati.txt
+53. echo "top Command" >> sharonGarapati.txt
+54. echo "===========" >> sharonGarapati.txt
+55. echo "" >> sharonGarapati.txt
+56. top >> sharonGarapati.txt
+57. echo "" >> sharonGarapati.txt
+58. echo "===============" >> sharonGarapati.txt
+59. echo "history Command" >> sharonGarapati.txt
+60. echo "===============" >> sharonGarapati.txt
+61. echo "" >> sharonGarapati.txt
+62. history >> sharonGarapati.txt
+63. echo "" >> sharonGarapati.txt
+64. echo "End of the Script" >> sharonGarapati.txt
